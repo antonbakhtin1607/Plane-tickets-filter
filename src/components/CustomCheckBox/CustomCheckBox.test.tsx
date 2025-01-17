bc0f2de77b05cdd6a839abfe2a0d1567fa4c8f63
antonbakhtin1607 <antonbakhtin1607@gmail.com>
2025-01-17 14:24:51 +0200

    added snapshot testing for CustomCheckBox component

--- src/components/CustomCheckBox/CustomCheckBox.test.tsx
+++ src/components/CustomCheckBox/CustomCheckBox.test.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import CustomCheckbox from './CustomCheckbox';
 
 describe('CustomCheckbox Component', () => {
   const mockOnChange = jest.fn();
 
   it('renders the checkbox with the correct label', () => {
-    render(<CustomCheckbox checked={false} label="Test Label" onChange={mockOnChange} />);
+    const { asFragment } = render(<CustomCheckbox checked={false} label="Test Label" onChange={mockOnChange} />);
+    
+    expect(asFragment()).toMatchSnapshot();
+
     expect(screen.getByLabelText('Test Label')).toBeInTheDocument();
   });
 
   it('applies the checked state correctly', () => {
-    render(<CustomCheckbox checked={true} label="Checked" onChange={mockOnChange} />);
+    const { asFragment } = render(<CustomCheckbox checked={true} label="Checked" onChange={mockOnChange} />);
+    
+    expect(asFragment()).toMatchSnapshot();
+
     const checkbox = screen.getByRole('checkbox');
     expect(checkbox).toBeChecked();
   });
 
   it('calls the onChange handler when clicked', () => {
-    render(<CustomCheckbox checked={false} label="Click Test" onChange={mockOnChange} />);
+    const { asFragment } = render(<CustomCheckbox checked={false} label="Click Test" onChange={mockOnChange} />);
+    
+    expect(asFragment()).toMatchSnapshot();
+
     const checkbox = screen.getByRole('checkbox');
     fireEvent.click(checkbox);
+    
+    expect(asFragment()).toMatchSnapshot();
+
     expect(mockOnChange).toHaveBeenCalledTimes(1);
   });
 });
